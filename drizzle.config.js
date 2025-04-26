487b7983c75de92ad2e3df1cafe0759f5a6ca961
ankit007 <ankitkum837@gmail.com>
2025-04-27 02:23:03 +0530

    "project update"

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+
+export default  {
+    schema: './configs/schema.js',
+    dialect: 'postgresql',
+    dbCredentials: {
+      url:'postgresql://neondb_owner:npg_zGu0Dtq1dcwi@ep-little-darkness-a4rb64jf-pooler.us-east-1.aws.neon.tech/Motocar?sslmode=require',
+    },
+  };
